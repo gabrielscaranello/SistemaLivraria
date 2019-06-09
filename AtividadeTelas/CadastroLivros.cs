576fff0a5601e473835698c9d2738bdd3488ae42
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2019-06-09 13:43:56 -0300

    Adicionado as telas

---
+++ AtividadeTelas/CadastroLivros.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadeTelas
+{
+    public partial class CadastroLivros : Form
+    {
+        public CadastroLivros()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AtividadeTelas/TelaPrincipal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadeTelas
+{
+    public partial class tela1 : Form
+    {
+        public tela1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lOCAÇÃOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        
+
+        }
+
+        private void btCadFuncionário_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btLocar_Click(object sender, EventArgs e)
+        {
+            Vendas telaVenda = new Vendas();
+            telaVenda.Show();
+
+       
+        }
+    }
+
+    internal class Form2
+    {
+    }
+}
