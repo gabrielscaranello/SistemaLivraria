d76efe0cb24c8288cbe4caa8b7ef98a05b5a1de5
Emanuel Schulz <emanuelschulz1999@gmail.com>
2019-06-10 20:52:35 -0300

    BD

---
+++ Control/Banco.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+
+namespace AtividadeTelas.controle
+{
+    class Banco
+    {
+
+        public void connectar()
+        {
+            string conexao = "Server=localhost;Database=livraria;Uid=root;Pwd=root";
+            MySqlConnection connection = new MySqlConnection(conexao);
+            connection.Open();
+        }
+
+
+
+    }
+
+
+}
