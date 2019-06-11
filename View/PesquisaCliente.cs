5f2dc0e25cee6e625c19fa0dd441c52103c26892
alex <alebortoloto@gmail.com>
2019-06-11 16:54:09 -0300

    Começando o select

--- View/PesquisaCliente.cs
+++ View/PesquisaCliente.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,10 @@ namespace SistemaBiblioteca
             InitializeComponent();
         }
 
+        SqlConnection sqlconn = null;
+        private String strCon = "Server=srv02.hostoo.io;Port=3306;Database=Sistemalivraria;Uid=Sistemalivraria;Pwd=B1n2&thcHhj9CdP4";
+        private String strSql = String.Empty;
+
         private void Label2_Click(object sender, EventArgs e)
         {
 
@@ -24,7 +29,7 @@ namespace SistemaBiblioteca
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            
+            strCon = "select * from livraria.Cliente where nome=@nome";
         }
     }
 }
