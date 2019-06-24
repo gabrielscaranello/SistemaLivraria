ea555c8bb267e3772349cb56478a463b9a213107
julio suzuki <suzukij332@.gmail.com>
2019-06-24 19:51:50 -0300

    insert cliente

--- Model/ClienteModel.cs
+++ Model/ClienteModel.cs
@@ -16,13 +16,13 @@ namespace AtividadeTelas.model
         private String email;
         private DateTime dataNasc;
 
-        protected int Id { get => id; set => id = value; }
-        protected string Nome { get => nome; set => nome = value; }
-        protected string Telefone { get => telefone; set => telefone = value; }
-        protected string Cpf { get => cpf; set => cpf = value; }
-        protected string Endereco { get => endereco; set => endereco = value; }
-        protected string Email { get => email; set => email = value; }
-        protected DateTime DataNasc { get => dataNasc; set => dataNasc = value; }
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Telefone { get => telefone; set => telefone = value; }
+        public string Cpf { get => cpf; set => cpf = value; }
+        public string Endereco { get => endereco; set => endereco = value; }
+        public string Email { get => email; set => email = value; }
+        public DateTime DataNasc { get => dataNasc; set => dataNasc = value; }
 
         public Cliente()
         {
