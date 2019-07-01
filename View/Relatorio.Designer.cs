b496f7996883f597a8c813c7735c2b2955f423bf
rafael <rpeerez18@gmail.com>
2019-07-01 12:35:32 -0300

    relatoio

--- View/Relatorio.Designer.cs
+++ View/Relatorio.Designer.cs
@@ -32,14 +32,16 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dgpTabRelatorio = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.a = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(713, 415);
+            this.button1.Location = new System.Drawing.Point(951, 511);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,28 +53,43 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dgpTabRelatorio,
             this.a});
-            this.dataGridView1.Location = new System.Drawing.Point(128, 136);
+            this.dataGridView1.Location = new System.Drawing.Point(1, 13);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(688, 358);
             this.dataGridView1.TabIndex = 1;
             // 
             // dgpTabRelatorio
             // 
             this.dgpTabRelatorio.HeaderText = "ID";
+            this.dgpTabRelatorio.MinimumWidth = 6;
             this.dgpTabRelatorio.Name = "dgpTabRelatorio";
+            this.dgpTabRelatorio.Width = 125;
             // 
             // a
             // 
             this.a.HeaderText = "Venda";
+            this.a.MinimumWidth = 6;
             this.a.Name = "a";
+            this.a.Width = 125;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "ReportViewer";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer1.TabIndex = 0;
             // 
             // Relatorio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Relatorio";
             this.Text = "Relatorio";
             this.Load += new System.EventHandler(this.Relatorio_Load);
@@ -87,5 +104,6 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dgpTabRelatorio;
         private System.Windows.Forms.DataGridViewTextBoxColumn a;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
     }
 }
